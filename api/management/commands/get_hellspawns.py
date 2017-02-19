460afc487ab4c6f77702c7beef44d4b2973df1d3
rapospectre <rapospectre@163.com>
2017-02-19 16:31:42 +0800

    <feat>  add http
    Author:rapospectre@gmail.com
    
    Desc: add http

--- api/management/commands/get_hellspawns.py
+++ api/management/commands/get_hellspawns.py
@@ -13,6 +13,8 @@ from core.models import Hellspawn
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        h = Hellspawn.objects.all()
+        h.delete()
         url = "https://g37simulator.webapp.163.com/get_heroid_list?callback=jQuery11130959811888616583_1487429691764&rarity=0&page=1&per_page=200&_=1487429691765"
         result = requests.get(url).content.replace('jQuery11130959811888616583_1487429691764(', '').replace(')', '')
         json_data = json.loads(result)
@@ -21,7 +23,7 @@ class Command(BaseCommand):
         for k, v in hellspawn_list.iteritems():
             file_name = p.get_pinyin(v.get('name'), '')
             print 'id: {0} name: {1}'.format(k, v.get('name'))
-            big_url = 'static.rapospectre.com/{0}@big.png'.format(file_name)
-            icon_url = 'static.rapospectre.com/{0}@icon.png'.format(file_name)
+            big_url = 'http://static.rapospectre.com/{0}@big.png'.format(file_name)
+            icon_url = 'http://static.rapospectre.com/{0}@icon.png'.format(file_name)
             rarity = {1: 4, 2: 3, 3: 2, 4: 1}
             Hellspawn(name=v.get('name'), picture=big_url, icon=icon_url, rarity=rarity[v.get('rarity')]).save()
